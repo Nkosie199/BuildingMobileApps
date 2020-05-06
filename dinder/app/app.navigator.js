e01aabe44237a9c28840020438741fb6c4e2e78a
nkosigumede <nkosi.gumede@easypay.co.za>
2020-05-06 23:57:28 +0200

    Saving at Ch05_01 (with errors)

--- dinder/app/app.navigator.js
+++ dinder/app/app.navigator.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createStackNavigator } from 'react-navigation-stack';
 import SplashScreen from './screens/splash.screen';
+import LoginScreen from './screens/login.screen';
+import MatchScreen from './screens/match.screen';
 
 const Splash = {
   screen: SplashScreen,
@@ -9,9 +12,28 @@ const Splash = {
     header: null
   }
 }
+const Login = {
+  screen: LoginScreen,
+  navigationOptions: {
+    header: null
+  }
+}
+const Match = {
+  screen: MatchScreen,
+  navigationOptions: {
+    headerMode: 'screen',
+    headerTitle: 'Matches',
+    drawerLabel: 'Matches'
+  }
+}
+const MatchStack = createStackNavigator({
+  Match: Match
+},{})
 
 const drawer = createDrawerNavigator({
-  Splash: Splash
+  Splash: Splash,
+  Login: Login,
+  Match: {screen: MatchStack}
 })
 
 const AppNavigator = createAppContainer(drawer);
--- dinder/app/stores/config.store.js
+++ dinder/app/stores/config.store.js
@@ -1,12 +1,31 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBxO_Iln2c_Li9Q1ygCN-wmZN2ovW5sXVw",
+  authDomain: "dinder-717eb.firebaseapp.com",
+  databaseURL: "https://dinder-717eb.firebaseio.com",
+  projectId: "dinder-717eb",
+  storageBucket: "dinder-717eb.appspot.com",
+  messagingSenderId: "1014745069034",
+  appId: "1:1014745069034:web:0ae21b45f8b2272cacee93",
+  measurementId: "G-5SPTMVS5LD"
+};
+
+
 export default class ConfigStore {
   constructor() {
+    firebase.initializeApp(config)
     this.splashTime = 1000
     this.splashImg = require('../../images/splash.jpg')
+    this.loginBG = require('../../images/login.jpg')
   }
   get SplashImg() {
     return this.splashImg
   }
   get SplashTime() {
     return this.splashTime
+  }
+  get LoginBG() {
+    return this.loginBG
   }
 }
