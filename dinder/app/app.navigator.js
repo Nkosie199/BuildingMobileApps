bb73bc25fc8cf68467026ed34d62531a9a0ffa51
nkosigumede <nkosi.gumede@easypay.co.za>
2020-05-07 20:34:00 +0200

    Completed tutorial. Must fix errors

--- dinder/app/app.navigator.js
+++ dinder/app/app.navigator.js
@@ -5,7 +5,38 @@ import { createStackNavigator } from 'react-navigation-stack';
 import SplashScreen from './screens/splash.screen';
 import LoginScreen from './screens/login.screen';
 import MatchScreen from './screens/match.screen';
+import PostScreen from './screens/post.screen';
+import {ScrollView } from 'react-native';
+import {
+  Button,
+  Icon
+} from 'native-base';
 
+const hiddenItems = [
+  'Splash',
+  'Login'
+];
+
+const SideBar = (props) => {
+  const propsClone = {
+    ...props,
+    items: props.items.filter(item => !hiddenItems.includes(item.key))
+  }
+  return (
+    <ScrollView>
+      <DrawerItems {...propsClone}/>
+    </ScrollView>
+  )
+}
+
+const MenuButton = ({navigate}) => (
+  <Button transparent 
+    onPress={() => {
+      navigate('DrawerOpen')
+    }}>
+    <Icon style={{color: "#fff"}} size={28} name="menu"/>
+  </Button>
+)
 const Splash = {
   screen: SplashScreen,
   navigationOptions: {
@@ -26,9 +57,24 @@ const Match = {
     drawerLabel: 'Matches'
   }
 }
+const Post = {
+  screen: PostScreen,
+  navigationOptions: {
+    headerMode: 'screen',
+    headerTitle: 'Post'
+  }
+}
 const MatchStack = createStackNavigator({
-  Match: Match
-},{})
+  Match: Match,
+  Post: Post
+},{
+  navigationOptions: ({navigation, HeaderProps}) => ({
+    headerLeft: () => <MenuButton navigate={navigation.navigate}/>,
+    headerStyle: { backgroundColor: "#000"},
+    headerTintColor: "#fff"
+  }) 
+})
+
 
 const drawer = createDrawerNavigator({
   Splash: Splash,
@@ -36,6 +82,13 @@ const drawer = createDrawerNavigator({
   Match: {screen: MatchStack}
 })
 
-const AppNavigator = createAppContainer(drawer);
+const RouteConfig = {
+  initialRoute: 'Splash',
+  contentComponent: SideBar,
+  navigationOptions: {
+    gesturesEnabled: false
+  }
+}
+const AppNavigator = createAppContainer(drawer, RouteConfig);
 
 export default AppNavigator;
